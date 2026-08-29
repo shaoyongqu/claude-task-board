8575431b14076c1ce9f743d890b190015f0d2628
曲绍墉 <shaoyongqu@163.com>
2026-08-30 07:45:04 +0800

    Hide the 'change status to todo' comment toggle when already in todo
    The comment-composer toggle that sends an issue back to todo (rework
    flow) only makes sense from another status; on a todo issue it rendered
    as a meaningless switch. Show it only when the current status is not
    todo.

--- web/src/components/TaskDetail.tsx
+++ web/src/components/TaskDetail.tsx
@@ -1514,19 +1514,21 @@ export function TaskDetail({
                     />
                   </div>
                   <div>
-                    <div className="comment-status-action">
-                      <span>{text("改变状态为-等待认领", "Change status to Todo")}</span>
-                      <button
-                        type="button"
-                        className={`board-setting-switch${changeStatusToTodo ? " is-on" : ""}`}
-                        role="switch"
-                        aria-checked={changeStatusToTodo}
-                        disabled={submitting}
-                        onClick={() => setChangeStatusToTodo((current) => !current)}
-                      >
-                        <span aria-hidden="true" />
-                      </button>
-                    </div>
+                    {currentTask.status !== "todo" && (
+                      <div className="comment-status-action">
+                        <span>{text("改变状态为-等待认领", "Change status to Todo")}</span>
+                        <button
+                          type="button"
+                          className={`board-setting-switch${changeStatusToTodo ? " is-on" : ""}`}
+                          role="switch"
+                          aria-checked={changeStatusToTodo}
+                          disabled={submitting}
+                          onClick={() => setChangeStatusToTodo((current) => !current)}
+                        >
+                          <span aria-hidden="true" />
+                        </button>
+                      </div>
+                    )}
                     <button
                       className="button primary"
                       type="submit"
